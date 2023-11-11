f517f39eb3a7c368018111dea5191ecf8f0d7298
Dean McNevin <deaner1456@gmail.com>
2023-11-11 12:01:26 -0600

    First 3 Problems Finished

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -6,16 +6,28 @@ console.log('****** Part Supply *******');
 // 1. Create a variable called 'partsNeeded' with a value of the number 40.
 console.log('1. Create partsNeeded:');
 
+//answer:
+let partsNeeded = 40;
+  console.log(partsNeeded);
+//end answer
 
 // 2. Create a variable called 'supplyChanges' whose value is an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11.
 console.log('2. Create supplyChanges:');
 
+//answer:
+let supplyChanges = [ 3, 5, -6, 0, 7, 11];
+  console.log(supplyChanges);
+//end answer
 
 // 3. Create a variable called 'secondItem' and assign it the value of the second
 //    item in the 'supplyChanges' array.
 console.log('3. Access the second value of supplyChanges:');
 
+//answer:
+let secondItem = supplyChanges[1];
+  console.log(secondItem);
+//end answer
 
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
